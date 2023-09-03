1553d76c526517cfa5502ba9b38273a0294ffdf1
RobertoAlvesBRA <90912098+RobertoAlvesBRA@users.noreply.github.com>
2023-09-03 11:24:10 -0300

    Caixa de Dialogo
    Para testar:
    - Adicione DialogoUI no Canvas
    - Adicione o DialogoController na cena
    - Faça a referencia dos elementos da UI no Controller
    - Atribua funções aos botões :  next e back.

---
+++ Assets/_Scripts/Dialogo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogo : MonoBehaviour
+{
+    public string nome;
+    public Sprite foto;
+    public bool fotoLadoDireito;
+    [TextArea]
+    public string[] messagens;
+
+}
