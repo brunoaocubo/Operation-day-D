e1947b7a6565f1722ac0f8e272695c24b11811fc
RobertoAlvesBRA <90912098+RobertoAlvesBRA@users.noreply.github.com>
2023-12-03 20:23:03 -0300

    Ajustes

--- Assets/_Scripts/HUD and Controller/Loading.cs
+++ Assets/_Scripts/HUD and Controller/Loading.cs
@@ -9,88 +9,60 @@ using TMPro;
 public class Loading : MonoBehaviour
 {
 
-    [Header("Video")]
-    [SerializeField] private GameObject loaderUI;
     [SerializeField] private Slider progressSlider;
 
 
-    //[Header("Config")]
-    //[SerializeField] private float delay;
-    //[SerializeField] private float delayText;
-    //[SerializeField] private float velocityRotation;
+    [Header("Config")]
+    [SerializeField] private float delay;
+    [SerializeField] private float delayText;
+    [SerializeField] private float velocityRotation;
 
-    //[Header("Link UI Components")]
-    //[SerializeField] private Image postit;
-    //[SerializeField] private Image circle;
-    //[SerializeField] private TextMeshProUGUI loadingText;
-    //[SerializeField] private GameObject loadingCanvasUI;
-    //private bool loadScreen = true;
+    [Header("Link UI Components")]
+    [SerializeField] private Image postit;
+    [SerializeField] private Image circle;
+    [SerializeField] private TextMeshProUGUI loadingText;
+    [SerializeField] private GameObject loadingCanvasUI;
+    private bool loadScreen = true;
 
-    //private float time;
+    private float time;
 
-
-    //private void Start()
-    //{
-    //    time = 0;
-    //    loadScreen = true;
-    //    StartCoroutine(AnimText());
-    //}
-
-    //private void Update()
-    //{
-    //    if (loadScreen)
-    //    {
-    //        time += Time.deltaTime;
-    //        circle.rectTransform.localEulerAngles -= new Vector3(0, 0, velocityRotation * Time.deltaTime);
-    //        if (time >= delay)
-    //        {
-    //            loadScreen = false;
-    //            loadingCanvasUI.SetActive(false);
-    //            this.enabled = false;
-    //        }
-    //    }
-    //}
-
-    //IEnumerator AnimText()
-    //{
-    //    for (int i = 0; loadScreen; i++)
-    //    {
-    //        loadingText.text = "Carregando.";
-    //        yield return new WaitForSeconds(delayText);
-    //        loadingText.text = "Carregando..";
-    //        yield return new WaitForSeconds(delayText);
-    //        loadingText.text = "Carregando...";
-    //        yield return new WaitForSeconds(delayText);
-    //    }
-    //}
 
     private void Start()
     {
-        StartCoroutine(LoadScene_Coroutine(0));
-    }
-    
-    IEnumerator LoadScene_Coroutine(int index)
-    {
-        Debug.Log("Executei");
+        time = 0;
         progressSlider.value = 0;
-        //loaderUI.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
-        asyncOperation.allowSceneActivation = false;
-        float progress = 0;
-        while (!asyncOperation.isDone)
-        {
-            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
-            progressSlider.value = progress;
-            if (progress >= 0.9f)
-            {
-                progressSlider.value = 1f;
-                asyncOperation.allowSceneActivation = true;
-            }
-
-
-        }
-        //yield return null;
+        progressSlider.maxValue = delay;
+        loadScreen = true;
+        StartCoroutine(AnimText());
+        
     }
 
+    private void Update()
+    {
+        if (loadScreen)
+        {
+            progressSlider.value = Mathf.MoveTowards(progressSlider.value, delay, Time.deltaTime);
+            time += Time.deltaTime;
+            circle.rectTransform.localEulerAngles -= new Vector3(0, 0, velocityRotation * Time.deltaTime);
+            if (time >= delay)
+            {
+                loadScreen = false;
+                loadingCanvasUI.SetActive(false);
+                this.enabled = false;
+            }
+        }
+    }
+
+    IEnumerator AnimText()
+    {
+        for (int i = 0; loadScreen; i++)
+        {
+            loadingText.text = "Carregando.";
+            yield return new WaitForSeconds(delayText);
+            loadingText.text = "Carregando..";
+            yield return new WaitForSeconds(delayText);
+            loadingText.text = "Carregando...";
+            yield return new WaitForSeconds(delayText);
+        }
+    }
 }
